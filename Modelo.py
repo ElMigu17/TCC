9e4ae6c1c29c25f64a7ac81a9ba4cf239dba248a
miguel.rgo <miguel.rodrigues@gafit.com>
2023-01-31 21:35:05 -0300

    Correção da otimização e acrescimo de otimização de horario

--- Modelo.py
+++ Modelo.py
@@ -11,28 +11,14 @@ class distribuicao_graduacao:
         self.limite_inferior = 8
         self.limite_superior_padrao = 16
         self.limite_superior_reduzido = 16
-        pass
 
-    # def leitura_disciplinas(self):
-    #     disciplinas = [
-    #         disciplina(0, 'GMM101', 2, [{ "dia_semana": 3, "hora_inicio": "13:00", "hora_fim": "14:40"}], True, ['9A']),
-    #         disciplina(1, 'GMM101', 4, [{ "dia_semana": 5, "hora_inicio": "19:00", "hora_fim": "20:40"}], True, ['26A']),
-    #         disciplina(2, 'GMM101', 4, [{ "dia_semana": 4, "hora_inicio": "13:00", "hora_fim": "14:40"}, { "dia_semana": 5, "hora_inicio": "13:00", "hora_fim": "14:40"}], True, ['13A', '10A', '21A']),
-    #         disciplina(3, 'GMM101', 2, [{ "dia_semana": 2, "hora_inicio": "8:00", "hora_fim": "9:40"}, { "dia_semana": 5, "hora_inicio": "15:00", "hora_fim": "16:40"}], True, ['30C', '30A']),
-    #         disciplina(4, 'GMM101', 6,
-    #         [{ "dia_semana": 1, "hora_inicio": "13:00", "hora_fim": "9:40"}, { "dia_semana": 3, "hora_inicio": "15:00", "hora_fim": "16:40"}, { "dia_semana": 5, "hora_inicio": "19:00", "hora_fim": "20:40"}],
-    #         True, ['30G', '30E'])
-    #     ]
+        self.peso_infracao_horario = 0.5
 
-    #     return disciplinas
+        self.restricao_horario_turnos = False
+        self.restricao_horario_23_18 = False
+        
 
-    # def leitura_docentes(self):
-    #     docentes = [
-    #         docente(0, "claudin", 123456),
-    #         docente(1, "roger", 654321)
-    #     ]
 
-    #     return docentes
 
     def leitura_disciplinas(self):
         am = array_manipulator()
@@ -90,38 +76,44 @@ class distribuicao_graduacao:
                 aula_hf = self.hora_para_float(aula["hora_fim"])
                 aula1_hf = self.hora_para_float(aula1["hora_fim"])
 
-                if (aula["dia_semana"] == aula1["dia_semana"]-1):
-                    if ((aula_hi >= 21) and aula1_hi <= 10):
-                        return  True
+                if self.restricao_horario_23_18:
+                    if (aula["dia_semana"] == aula1["dia_semana"]-1):
+                        if ((aula_hi >= 21) and aula1_hi <= 10):
+                            return  True
 
-                elif (aula1["dia_semana"] == aula["dia_semana"]-1):
-                    if ((aula1_hi >= 21) and aula_hi <= 10):
-                        return  True
+                    elif (aula1["dia_semana"] == aula["dia_semana"]-1):
+                        if ((aula1_hi >= 21) and aula_hi <= 10):
+                            return  True
 
-                if ((dis.pos != dis1.pos) and (aula["dia_semana"] == aula1["dia_semana"])):
+                if((dis.pos != dis1.pos) and (aula["dia_semana"] == aula1["dia_semana"])):
                     if (((aula_hi <= aula1_hi) and (aula1_hi <= aula_hf))
                       or ((aula1_hi <= aula_hi) and (aula_hi <= aula1_hf))):
                         return  True
                     
-                    elif ((aula_hi <= self.fim_turno_manha and aula1_hi >= self.inicio_turno_noite) or 
-                          (aula_hi >= self.inicio_turno_noite and aula1_hi <= self.fim_turno_manha)):
+                    elif(self.restricao_horario_turnos and
+                      ((aula_hi <= self.fim_turno_manha and aula1_hi >= self.inicio_turno_noite) or 
+                      (aula_hi >= self.inicio_turno_noite and aula1_hi <= self.fim_turno_manha))):
                         return  True
         return False
 
-    def res_horario(self):
-        
+    def todos_conflitos_horario(self) -> list:
         ids_pares_proibidos = []
         for i in range(len(self.disciplinas)):
             for j in range(i, len(self.disciplinas)):
                 dis = self.disciplinas
+
                 if self.ha_conflito_horario(dis[i], dis[j]):
                     ids_pares_proibidos.append([dis[i].pos, dis[j].pos])
+        return ids_pares_proibidos
+
+    def res_horario(self):
+        ids_pares_proibidos = self.todos_conflitos_horario()
 
         for doc in self.docentes:
             for par in ids_pares_proibidos:
                 self.modelo.Add((self.atribuicao[(doc.pos, par[0])] + self.atribuicao[(doc.pos, par[1])]) <= 1)
 
-    def res_preferencia(self):
+    def res_prioridade(self):
         for doc in self.docentes:
             for pre in doc.preferencia:
                 if pre in doc.disc_per_1 and not ( pre in doc.disc_per_2 and pre in doc.disc_per_3 ):
@@ -131,14 +123,88 @@ class distribuicao_graduacao:
 
 
     def opt_interesse(self):
+        pref_disc = 0
 
         for doc in self.docentes:
-            pref_disc = 0
             for dis in self.disciplinas:
                 if dis.codigo in doc.preferencia:
                     pref_disc += (self.atribuicao[(doc.pos, dis.pos)] * doc.preferencia[dis.codigo])
-            self.modelo.Maximize(pref_disc)
+        return pref_disc
 
+    def opt_horarios(self):
+        aux_restricao_horario_23_18 = not self.restricao_horario_23_18
+        aux_restricao_horario_turnos = not self.restricao_horario_turnos
+        self.restricao_horario_23_18 = True
+        self.restricao_horario_turnos = True
+
+        soma_peso = 0
+
+        ids_pares_proibidos = self.todos_conflitos_horario()
+        for doc in self.docentes:
+            for par in ids_pares_proibidos:
+                soma_peso += -self.atribuicao[(doc.pos, par[0])] + -self.atribuicao[(doc.pos, par[1])]
+        
+        self.restricao_horario_23_18 = aux_restricao_horario_23_18        
+        self.restricao_horario_turnos = aux_restricao_horario_turnos
+
+        return soma_peso
+
+    def exibe_solucao_achada(self, solver):
+        qtd_preferencias = 0
+        qtd_preferencias_peso = 0
+        array_creditos = []
+
+        for doc in self.docentes:
+            array_creditos.append(0)
+
+            for dis in self.disciplinas:
+                if solver.Value(self.atribuicao[(doc.pos, dis.pos)]) == 1:
+                    add = ""
+                    if dis.string_cod_turma() in doc.preferencia:
+                        
+                        add = "que possui preferencia " + str(doc.preferencia[dis.string_cod_turma()])
+                        qtd_preferencias += 1
+                        qtd_preferencias_peso += doc.preferencia[dis.string_cod_turma()]
+                    print('Docente', doc.pos, 'lecionara a disciplina',  dis.pos, add)
+                    array_creditos[-1] += dis.qtd_creditos
+                    doc.disciplinas.append(dis.string_cod_turma())
+            print("Quantidade total de creditos:", array_creditos[-1])
+
+            print()
+        print('Quantidade de preferencias atendidas =', qtd_preferencias, qtd_preferencias_peso)
+        media_creditos = sum(array_creditos)/len(self.docentes)
+        print('Media de créditos: ', media_creditos)
+        variancia = sum((a-media_creditos)*(a-media_creditos) for a in array_creditos)/(len(self.docentes)-1)
+        print('Variancia de créditos: ', variancia)
+        print('Desvio padrão de créditos: ', math.sqrt(variancia))
+
+        am = array_manipulator()
+        am.save_as_json(self.docentes, True)
+        
+
+    def verifica_solucao(self):
+        solver = cp_model.CpSolver()
+        #solver.parameters.log_search_progress = True
+
+        status = solver.Solve(self.modelo)
+
+        if status == cp_model.OPTIMAL:
+            print('Optimal solution:')
+
+        elif status == cp_model.FEASIBLE:
+            print('Feasible solution:')
+
+        if status == cp_model.OPTIMAL or status == cp_model.FEASIBLE:
+            self.exibe_solucao_achada(solver)
+
+        else:
+            print('No solution found !')
+            
+        print('\nStatistics')
+        print('  - conflicts: %i' % solver.NumConflicts())
+        print('  - branches : %i' % solver.NumBranches())
+        print('  - wall time: %f s' % solver.WallTime())
+        
         
     def calcula(self):
         self.disciplinas = self.leitura_disciplinas()
@@ -148,63 +214,15 @@ class distribuicao_graduacao:
 
         self.res_limites_creditos()
         self.res_um_doc_por_dis()
-        #self.res_horario()
-        self.res_preferencia()
-        self.opt_interesse()
+        self.res_horario()
+        self.res_prioridade()
+        
+        soma_opt = self.opt_interesse()
+        soma_opt += self.opt_horarios() * self.peso_infracao_horario
+        self.modelo.Maximize(soma_opt)
 
+        self.verifica_solucao()
 
-        solver = cp_model.CpSolver()
-        #solver.parameters.log_search_progress = True
-        status = solver.Solve(self.modelo)
-
-
-        if status == cp_model.OPTIMAL:
-            print('Optimal solution:')
-        elif status == cp_model.FEASIBLE:
-            print('Feasible solution:')
-
-        if status == cp_model.OPTIMAL or status == cp_model.FEASIBLE:
-            qtd_preferencias = 0
-            qtd_preferencias_peso = 0
-            array_creditos = []
-
-            for doc in self.docentes:
-                array_creditos.append(0)
-
-                for dis in self.disciplinas:
-                    if solver.Value(self.atribuicao[(doc.pos, dis.pos)]) == 1:
-                        add = ""
-                        if dis.string_cod_turma() in doc.preferencia:
-                            
-                            add = "que possui preferencia " + str(doc.preferencia[dis.string_cod_turma()])
-                            qtd_preferencias += 1
-                            qtd_preferencias_peso += doc.preferencia[dis.string_cod_turma()]
-                        print('Docente', doc.pos, 'lecionara a disciplina',  dis.pos, add)
-                        array_creditos[-1] += dis.qtd_creditos
-                        doc.disciplinas.append(dis.string_cod_turma())
-                print("Quantidade total de creditos:", array_creditos[-1])
-
-                print()
-            print('Quantidade de preferencias atendidas =', qtd_preferencias, qtd_preferencias_peso)
-            media_creditos = sum(array_creditos)/len(self.docentes)
-            print('Media de créditos: ', media_creditos)
-            variancia = sum((a-media_creditos)*(a-media_creditos) for a in array_creditos)/(len(self.docentes)-1)
-            print('Variancia de créditos: ', variancia)
-            print('Desvio padrão de créditos: ', math.sqrt(variancia))
-
-
-            am = array_manipulator()
-            am.save_as_json(self.docentes, True)
-            
-
-
-        else:
-            print('No solution found !')
-            
-        print('\nStatistics')
-        print('  - conflicts: %i' % solver.NumConflicts())
-        print('  - branches : %i' % solver.NumBranches())
-        print('  - wall time: %f s' % solver.WallTime())
 
 
 
