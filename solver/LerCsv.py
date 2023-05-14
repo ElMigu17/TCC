5f0ad10b3100d214cd1d5e2375b48d4d53d8700c
miguel.rgo <miguel.rodrigues@gafit.com>
2023-05-14 11:39:29 -0300

    Limpeza

--- solver/LerCsv.py
+++ solver/LerCsv.py
@@ -2,17 +2,53 @@ from solver.Estruturas_de_Dados import disciplina, docente, array_manipulator
 
 class leitor_csv:
     def __init__(self) -> None:
-        self.dia_num = {"Segunda-Feira" : 1,
-            "Terça-Feira" : 2,
-            "Quarta-Feira" : 3,
-            "Quinta-Feira" : 4,
-            "Sexta-Feira" : 5,
-            "Sábado" : 6}
+        self.dia_num = {"Seg" : 1,
+            "Ter" : 2,
+            "Qua" : 3,
+            "Qui" : 4,
+            "Sex" : 5,
+            "Sáb" : 6,
+            "Sab" : 6}
 
         self.docentes = []
         self.siape_docente = {}
         self.disciplinas = []
         self.docentes_not_found = {}
+
+    def cria_prox_disciplina(self, Dados_Gerais, i):
+        def descobre_qtd_creditos(horarios):
+            if(horarios[1].split(":")[1] == '50'):
+                return 1
+            elif(horarios[1].split(":")[1] == '40'):
+                return 2
+            else:
+                return 3
+
+        dado = Dados_Gerais[i]
+        try:
+            dia_semana = self.get_pos_weekday([dado[5]])
+        except Exception as e:
+            raise ValueError(e.args[0] + "na linha " + str(i))  
+        
+        aux_disciplina = {}
+        horarios = dado[6].split(' - ')
+        aux_disciplina['horarios'] = [{ "dia_semana": dia_semana, "hora_inicio": horarios[0], "hora_fim": horarios[1]}]
+        aux_disciplina['disciplina'] = dado[0]
+        aux_disciplina['turmas'] = [dado[7]]
+        aux_disciplina['docente'] = dado[14]
+        aux_disciplina['qtd_creditos'] = descobre_qtd_creditos(horarios)
+        i += 1
+        while i < len(Dados_Gerais) and len(Dados_Gerais[i]) >2 and Dados_Gerais[i][0] == '':
+            aux_disciplina['turmas'].append(Dados_Gerais[i][7])
+            i += 1
+
+        return aux_disciplina, i
+
+    def get_pos_weekday(self, this_dia):
+        for d in self.dia_num:
+            if d in this_dia[0]:
+                return self.dia_num[d]
+        raise ValueError("Erro na nomeação do dia da semana")
     
     def num_ou_zero(self, num):
         try:
@@ -31,31 +67,10 @@ class leitor_csv:
 
         aux_disciplina = {}
         id_i = 0
-
-        def descobre_qtd_creditos(horarios):
-            if(horarios[1].split(":")[1] == '50'):
-                return 1
-            else:
-                return 2
-            
-        def cria_prox_disciplina(i):
-            dado = Dados_Gerais[i]
-            aux_disciplina = {}
-            horarios = dado[6].split(' - ')
-            aux_disciplina['horarios'] = [{ "dia_semana": self.dia_num[dado[5]], "hora_inicio": horarios[0], "hora_fim": horarios[1]}]
-            aux_disciplina['disciplina'] = dado[0]
-            aux_disciplina['turmas'] = [dado[7]]
-            aux_disciplina['qtd_creditos'] = descobre_qtd_creditos(horarios)
-            i += 1
-            while i < len(Dados_Gerais) and len(Dados_Gerais[i]) >2 and Dados_Gerais[i][0] == '':
-                aux_disciplina['turmas'].append(Dados_Gerais[i][7])
-                i += 1
-
-            return aux_disciplina, i
         
-        prox_disciplina, index = cria_prox_disciplina(0)
+        prox_disciplina, index = self.cria_prox_disciplina(Dados_Gerais, 0)
         while len(Dados_Gerais) > index and len(Dados_Gerais[index]) > 2:
-            aux_disciplina, index = cria_prox_disciplina(index)
+            aux_disciplina, index = self.cria_prox_disciplina(Dados_Gerais, index)
             if(aux_disciplina["disciplina"] == prox_disciplina["disciplina"] and 
                aux_disciplina["turmas"] == prox_disciplina["turmas"]):
 
@@ -125,22 +140,11 @@ class leitor_csv:
             if docente_alvo == None:
                 self.docentes_not_found[semestre].append(str_disc_turma)
             
-        def cria_prox_disciplina(i):
-            dado = Dados_Gerais[i]
-            aux_disciplina = {}
-            aux_disciplina['disciplina'] = dado[0]
-            aux_disciplina['docente'] = dado[14]
-            aux_disciplina['turmas'] = [dado[7]]
-            i += 1
-            while i < len(Dados_Gerais) and len(Dados_Gerais[i]) >2 and Dados_Gerais[i][0] == '':
-                aux_disciplina['turmas'].append(Dados_Gerais[i][7])
-                i += 1
 
-            return aux_disciplina, i
         
-        prox_disciplina, index = cria_prox_disciplina(0)
+        prox_disciplina, index = self.cria_prox_disciplina(Dados_Gerais, 0)
         while len(Dados_Gerais) > index and len(Dados_Gerais[index]) > 2:
-            aux_disciplina, index = cria_prox_disciplina(index)
+            aux_disciplina, index = self.cria_prox_disciplina(Dados_Gerais, index)
             if(aux_disciplina["disciplina"] == prox_disciplina["disciplina"] and 
                aux_disciplina["turmas"] == prox_disciplina["turmas"]):
                 aux_disciplina = {}
@@ -151,7 +155,7 @@ class leitor_csv:
                 prox_disciplina = aux_disciplina
                 aux_disciplina = {}
                 id_i += 1
-        
+        print(self.disciplinas)
         tenta_add_turma_a_docente(prox_disciplina)
 
 
@@ -177,7 +181,7 @@ class leitor_csv:
             self.siape_docente[dosc_dados[0]] = index
             index += 1 
 
-    def importa_dados_passados(self, caminho_arquivo):
+    def importa_quantidades_passado(self, caminho_arquivo):
         ultimo_semestre = ""
 
         with open(caminho_arquivo, 'r') as file:
@@ -227,15 +231,36 @@ class leitor_csv:
              ultimo_semestre, 
              penultimo_semestre, 
              antipenultimo_semestre):
-        self.importa_dados_profs(dados_profs)
-        self.importa_dados_passados(dados_passado)
-        self.importa_dados_disciplinas(dados_disciplinas)
-        self.importa_preferencias(preferencias)
-        self.disciplinas_lecionadas_anteriormente(ultimo_semestre, 'disc_per_1')
-        self.disciplinas_lecionadas_anteriormente(penultimo_semestre, 'disc_per_2')
-        self.disciplinas_lecionadas_anteriormente(antipenultimo_semestre, 'disc_per_3')
+        funcoes = [ 
+            self.importa_dados_profs, 
+            self.importa_dados_disciplinas,
+            self.importa_quantidades_passado,
+            self.importa_preferencias,
+            self.disciplinas_lecionadas_anteriormente,
+            self.disciplinas_lecionadas_anteriormente,
+            self.disciplinas_lecionadas_anteriormente
+        ]
+        parametros =[
+            (dados_profs),
+            (dados_disciplinas),
+            (dados_passado),
+            (preferencias),
+            (ultimo_semestre, 'disc_per_1'),
+            (penultimo_semestre, 'disc_per_2'),
+            (antipenultimo_semestre, 'disc_per_3')
+        ]
 
-        #print(self.docentes_not_found)
+        for i in range(len(funcoes)):
+            try:
+                if len(parametros[i]) == 2:
+                    funcoes[i](parametros[i][0], parametros[i][1])
+                else:
+                    funcoes[i](parametros[i])
+            except ValueError as e:
+                raise ValueError( e.args[0] + " \nNa função: "+ funcoes[i].__name__ + " \nCom parametros: " + parametros[i])
+            except Exception as e:
+                raise Exception(e)
+
 
 
 if __name__ == '__main__':
